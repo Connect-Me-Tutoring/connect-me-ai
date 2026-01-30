3cf68ebcf111c31d8fdeb142dfb2ddfe5b0a5d3f
kuan <rueikuanwei@gmail.com>
2026-01-30 13:59:55 -0800

    impl. auth without api key

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,27 @@
 import { Hono } from "hono";
+import { bearerAuth } from 'hono/bearer-auth';
 import { dmAgent, generalAgent } from "./agents/agent-config";
 
-const app = new Hono();
+const callGeneralAgent = async (query: string) => {
+  const response = await generalAgent.generate(query);
+  return response.text;
+};
+
+const callDmAgent = async (query: string) => {
+  const response = await dmAgent.generate(query);
+  return response.text;
+};
+
+type Bindings = {
+  API_TOKEN: string;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
+
+app.use("/process-*", async (c, next) => {
+  const auth = bearerAuth({ token: c.env.API_TOKEN});
+  return auth(c, next);
+});
 
 app.get("/", async (c) => {
   return c.text("Hono App Is Working");
@@ -14,6 +34,7 @@ app.post("/process-dm", async (c) => {
     return response ? c.text(response) : c.text("No response");
   } catch (error) {
     console.error("Read Item Exception", error);
+    return c.text("Internal Server Error", 500);
   }
 });
 
@@ -28,14 +49,9 @@ app.post("/process-general", async (c) => {
   }
 });
 
-const callGeneralAgent = async (query: string) => {
-  const response = await generalAgent.generate(query);
-  return response.text;
-};
-
-const callDmAgent = async (query: string) => {
-  const response = await dmAgent.generate(query)
-  return response.text
-}
-
 export default app;
+    
+// export default {
+//   port: 1234,
+//   fetch: app.fetch,
+// };
